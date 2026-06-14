54a45779b3d8121b3e18c56f8c65d39375625aed
Mnari <mnpinvest@yahoo.com>
2026-06-14 23:55:57 +0200

    add sentry init frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+import * as Sentry from "@sentry/react";
+
+// Initialisation Sentry (à mettre tout en haut)
+Sentry.init({
+  dsn: "https://e00b4bec61ebb1a6ad1a56cfa5ea9f2f@o4511563741855744.ingest.de.sentry.io/4511565988757584",
+  tracesSampleRate: 1.0,
+});
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
